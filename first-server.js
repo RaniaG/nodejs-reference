841896444aa3abb0f00e2a5f71c07026eef5fdc0
Rania Gamal <rania.gamal@vodafone.com>
2022-08-14 12:58:04 +0200

    form response handling

--- first-server.js
+++ first-server.js
@@ -1,8 +1,27 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req,res)=>{
-    console.log(req.headers);
-    process.exit();
-})
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write("<body><h1> default response </h1></body>");
+  } else if (req.url.includes("answer") && req.method === "POST") {
+    req.on("data", (chunk) => {
+        fs.writeFileSync("./message", chunk);
+      });
+    res.setHeader("Location", "/");
+    res.writeHead(302);
+    res.write("<html>");
 
-server.listen('3000');
+  } else {
+    res.write("<html>");
+    res.write(
+      `<body><form method="POST" action="/answer"><input type="text" name="answer"/> <button type="submit"> submit</button> </form></body>`
+    );
+  }
+  res.write("</html>");
+  res.end();
+});
+
+server.listen("3000");
